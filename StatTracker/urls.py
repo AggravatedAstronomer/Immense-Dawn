839fee87b94dad93721882e96dc6a03254db808b
Alex Lale <alex.lale@isotoma.com>
2015-11-13 16:49:50 +0000

    Riot app registration verification

--- StatTracker/urls.py
+++ StatTracker/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.generic.base import TemplateView
 
 from . import views
 
@@ -8,5 +9,6 @@ urlpatterns = [
     #url(r'^summoner/(?P<summoner_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^name/$', views.search_summoner, name='search_summoner'),
     url(r'^harvested/(?P<task_id>.*)/$', views.harvested, name='harvested'),
-    url(r'^is_complete/(?P<task_id>.*)/$', views.is_complete, name='is_complete')
+    url(r'^is_complete/(?P<task_id>.*)/$', views.is_complete, name='is_complete'),
+    url(r'^riot\.txt$', TemplateView.as_view(template_name='StatTracker/riot.txt', content_type="text/plain")),
 ]
